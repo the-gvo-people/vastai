77259631dff21b4ff6e63610caa6fcc7ac97f77b
firminkam <gvo1112@gmail.com>
2025-03-17 23:23:08 +0000

    tesst

--- vast-cli/provisioning_script.sh
+++ vast-cli/provisioning_script.sh
@@ -13,13 +13,15 @@ cd "$WORKSPACE_DIR"
 # Activate the main virtual environment
 . /venv/main/bin/activate
 
+git clone https://gvo-dot:${GITHUB_TOKEN}@github.com/the-gvo-people/task_${TASK_ID}.git
 
-wget https://raw.githubusercontent.com/the-gvo-people/vastai/refs/heads/main/vast-cli/requirements.txt
 
-pip install -r requirements.txt
+#wget https://raw.githubusercontent.com/the-gvo-people/vastai/refs/heads/main/vast-cli/requirements.txt
+
+#pip install -r requirements.txt
 pip install jsonlines
 pip install openai
-pip install flash-attn --no-build-isolation
+#pip install flash-attn --no-build-isolation
 
 
 # Reload Supervisor
